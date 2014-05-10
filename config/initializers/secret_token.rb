2be38bd22a0bb4323c7321f6c17556fb70c39bbd
Luis Bajana <lbajana@gmail.com>
2014-05-10 00:36:26 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Contentio::Application.config.secret_token = '8ca8a2a0d105e935df8a9c395174c24d2ddee599c6a0a84c16f77881c5199eeb744deac686f5d4bcb2ee1795f8f2efd20701d9fb103d9ccef59407b8abde6a7e'
